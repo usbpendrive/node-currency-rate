1ba36832c59f33aa14a7acb34dd7658d81107f12
usbpendrive <ferry.fernando@gmail.com>
2018-07-09 08:03:26 +0700

    Axios api get testing.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { getRates } = require('./lib/fixer-service');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,3 +13,10 @@ app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.listen(port, () => {
     console.log('listening on %d', port);
 });
+
+const test = async() => {
+    const data = await getRates();
+    console.log(data);
+};
+
+test();
