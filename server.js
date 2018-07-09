52b0e4e02c30023c2d22aa392155ab22437b3ef8
usbpendrive <ferry.fernando@gmail.com>
2018-07-09 07:38:18 +0700

    Create index.html for application base index.

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.listen(port, () => {
+    console.log('listening on %d', port);
+});
