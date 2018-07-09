94a8ec3797b261f0273857a1cde140221a40c64e
usbpendrive <ferry.fernando@gmail.com>
2018-07-09 07:53:25 +0700

    Vanilla router navigation setup.

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,50 @@ window.addEventListener('load', () => {
 
     const errorTemplate = Handlebars.compile($('#error-template').html());
     const ratesTemplate = Handlebars.compile($('#rates-template').html());
+    const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
     const historicalTemplate = Handlebars.compile($('#historical-template').html());
 
-    const html = ratesTemplate();
-    el.html(html);
+    const router = new Router({
+        mode: 'history',
+        page404: (path) => {
+            const html = errorTemplate({
+                color: 'yellow',
+                title: 'Error 404 - Page NOT Found!',
+                message: `The path '/${path}' does not exist on this site`,
+            });
+            el.html(html);
+        },
+    });
+
+    router.add('/', () => {
+        let html = ratesTemplate();
+        el.html(html);
+    });
+
+    router.add('/exchange', () => {
+        let html = exchangeTemplate();
+        el.html(html);
+    });
+
+    router.add('/historical', () => {
+        let html = historicalTemplate();
+        el.html(html);
+    });
+
+    router.navigateTo(window.location.pathname);
+
+    const link = $(`a[href$='${window.location.pathname}']`);
+    link.addClass('active');
+
+    $('a').on('click', (event) => {
+        event.preventDefault();
+
+        const target = $(event.target);
+        $('.item').removeClass('active');
+        target.addClass('active');
+
+        const href = target.attr('href');
+        const path = href.substr(href.lastIndexOf('/'));
+        router.navigateTo(path);
+    });
 });
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 app.listen(port, () => {
     console.log('listening on %d', port);
 });
