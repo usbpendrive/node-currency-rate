94ca645d801369beff1b947bcbd9c2a70a3e50f0
usbpendrive <ferry.fernando@gmail.com>
2018-07-09 08:08:13 +0700

    API endpoint.

--- server.js
+++ server.js
@@ -8,15 +8,37 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+const errorHandler = (err, req, res) => {
+    if (err.response) {
+        res.status(403).send({ title: 'Server responded with an error', message: err.message });
+    } else if (err.request) {
+        res.status(503).send({ title: 'Unable to communicate with server', message: err.message });
+    } else {
+        res.status(500).send({ title: 'An unexpected error occured', message: err.message });
+    }
+};
+
+app.get('/api/rates', async (req, res) => {
+   try {
+       const data = await getRates();
+       res.setHeader('Content-Type', 'application/json');
+       res.send(data);
+   } catch (error) {
+       errorHandler(error, req, res);
+   }
+});
+
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => {
     console.log('listening on %d', port);
 });
 
+/*
 const test = async() => {
     const data = await getRates();
     console.log(data);
 };
 
 test();
+*/
